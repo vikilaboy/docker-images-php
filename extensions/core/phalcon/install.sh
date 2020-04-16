57338a50afcf42a5a81b0645fe3a03294079c838
El Nino <victor@niculae.net>
2020-04-16 04:04:08 +0300

    added phalcon extension

---
+++ extensions/core/phalcon/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+export PECL_EXTENSION=phalcon
+
+../docker-install.sh
---
+++ extensions/core/psr/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+export PECL_EXTENSION=psr
+
+../docker-install.sh
